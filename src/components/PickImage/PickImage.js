e4807ebb7524925c023610619566359a4c5c8ef5
Saurav Datta <sauravdatta06@gmail.com>
2021-10-03 23:46:12 +0600

    2nd update

---
+++ src/components/PickImage/PickImage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Button } from 'react-native';
+
+
+const PickImage = props => {
+    return (
+        <View>
+            <Button title="Pick an Image" />
+        </View>
+    );
+}
+
+export default PickImage;
--- src/components/SharePlaces/SharePlaces.js
+++ src/components/SharePlaces/SharePlaces.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import InputPlace from '../InputPlace/InputPlace';
+import { addPlace } from '../../redux/actionCreators';
+import { connect } from 'react-redux';
+import { View, Button } from 'react-native';
+import PickImage from '../PickImage/PickImage';
 
-const SharePlaces = () => {
+
+
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addPlace: place => dispatch(addPlace(place))
+    }
+}
+
+const SharePlaces = props => {
+    const [inputValue, setInputValue] = useState("");
     return (
         <View>
-            <Text>Share Places</Text>
+            <PickImage />
+            <InputPlace
+                inputValue={inputValue}
+                setInputValue={setInputValue}
+                addPlace={props.addPlace}
+            />
+
+            <View style={{
+                alignItems: "center"
+            }}>
+                <Button
+                    title="Add"
+                    onPress={() => {
+                        if (inputValue !== "") {
+                            props.addPlace({
+                                key: Math.random().toString(),
+                                value: props.inputValue,
+                                image: {
+                                    uri: "https://cdn.britannica.com/97/189797-050-1FC0041B/Night-view-Dhaka-Bangladesh.jpg"
+                                }
+                            });
+                            setInputValue("");
+                        }
+                    }}
+                />
+            </View>
+
         </View>
+
     );
 }
 
-export default SharePlaces;
+export default connect(null, mapDispatchToProps)(SharePlaces);
